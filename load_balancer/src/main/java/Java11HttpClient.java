92f0ddebaa383f6f0dfffb50368c1c503c66116b
Ayush Shukla <ericdemo07@gmail.com>
2021-08-22 15:58:16 +0530

    adding sync and async api call

--- load_balancer/src/main/java/Java11HttpClient.java
+++ load_balancer/src/main/java/Java11HttpClient.java
@@ -1,2 +1,40 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
 public class Java11HttpClient {
+
+    public APODModel makeClientApiCallSync(String urlAsString) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(urlAsString))
+                                         .header("accept", "application/json")
+                                         .build();
+
+        HttpResponse<Supplier<APODModel>> response = client.send(request, new JsonBodyHandler<>(APODModel.class));
+
+        return response.body().get();
+    }
+
+    public APODModel makeClientApiCallASync(String urlAsString) throws InterruptedException, ExecutionException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(urlAsString))
+                                         .header("accept", "application/json")
+                                         .build();
+
+        CompletableFuture<HttpResponse<Supplier<APODModel>>> responseFuture = client.sendAsync(request,
+                                                                                               new JsonBodyHandler<>(
+                                                                                                       APODModel.class));
+
+
+        HttpResponse<Supplier<APODModel>> response = responseFuture.get();
+
+        return response.body().get();
+    }
 }
--- load_balancer/src/test/java/ClientCallTest.java
+++ load_balancer/src/test/java/ClientCallTest.java
@@ -1,19 +1,56 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class ClientCallTest {
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
-    private static Java8HttpClient java8HttpClient;
+public class ClientCallTest {
+    private static Java8HttpClient  java8HttpClient;
+    private static Java11HttpClient java11HttpClient;
 
     @BeforeAll
     static void setup() {
         java8HttpClient = new Java8HttpClient();
+        java11HttpClient = new Java11HttpClient();
     }
 
     @Test
-    public void testClientApiCall() {
-        APODModel o = java8HttpClient.makeClientApiCall("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+    public void testClientApiCallJava8() {
+        APODModel apodModel = java8HttpClient.makeClientApiCall("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
 
-        System.out.println(o);
+        Assertions.assertTrue("David A. HardyPPARC".equals(apodModel.copyright));
+    }
+
+    @Test
+    public void testSyncClientApiCallJava11() {
+        APODModel apodModel = null;
+        try {
+            apodModel = java11HttpClient.makeClientApiCallSync("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertTrue("David A. HardyPPARC".equals(apodModel.copyright));
+    }
+
+    @Test
+    public void testAsyncClientApiCallJava11() {
+        APODModel apodModel = null;
+        try {
+            apodModel = java11HttpClient.makeClientApiCallASync("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertTrue("David A. HardyPPARC".equals(apodModel.copyright));
     }
 }
